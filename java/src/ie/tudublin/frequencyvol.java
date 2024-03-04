f7a7fc991867520cf8fb36e72b0516101e359fc2
Teni <teni@Tenis-MacBook-Air.local>
2024-03-04 17:57:50 +0000

    frequency detector added

---
+++ java/src/ie/tudublin/frequencyvol.java
@@ -0,0 +1,56 @@
+//Code to check if we are in the chorus or in the main
+
+package ie.tudublin;
+import processing.core.PApplet;
+import processing.core.PImage;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+
+
+import ddf.minim.*;
+import ddf.minim.analysis.*;
+public class frequencyvol extends PApplet{
+
+Minim minim;
+AudioPlayer song;
+FFT fft;
+float volumeThreshold = 0.27f; // 0.27 seems to be the sweet spot for detecting beats 
+PImage verseImage, chorusImage;
+
+public void settings() {
+  size(800, 600);
+  minim = new Minim(this);
+  song = minim.loadFile("java/data/Jb.mp3", 1024);
+  fft = new FFT(song.bufferSize(), song.sampleRate());
+  song.play();
+
+}
+
+public void draw() {
+  background(0);
+
+  fft.forward(song.mix);
+  float volume = 0;
+
+  //loop to calculates the volume /average amplitude of the audio signals in the current buffer
+  for(int i = 0; i < song.bufferSize(); i++) {
+    //abs is used  function,  used to convert the amplitude value to absolute value
+    volume += abs(song.mix.get(i));
+  }
+  volume /= song.bufferSize();
+
+  // condition to check for chorus vs verse
+  if (volume > volumeThreshold) {
+  
+    println("Chorus Detected!"); 
+  } else {
+    println("We are not in the chorus");
+  }
+}
+
+
+
+    
+}
